93e1927d716fa89793a213cb9c47570025ef8c61
Priti <Roypriti921@gmail.com>
2019-03-21 09:57:20 +0530

    completed

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+var meridiem = "AM"
+
 window.onload = (function() {
 			function displayTime() {
 				var currentTime = new Date();
@@ -6,8 +8,17 @@ window.onload = (function() {
 				var seconds = currentTime.getSeconds();
 				setInterval(displayTime, 1000);
 
+				if(hours > 12) {
+					hours = hours - 12;
+					meridiem = "PM";
+				}
+
+				if(hours === 0) {
+					hours = 12;
+				}
+
 				var clockTime = document.getElementById('clock');
-				clockTime.innerHTML = hours + ":" + minutes + ":" + seconds;
+				clockTime.innerHTML = hours + ":" + minutes + ":" + seconds + " " + meridiem;
 
 			}
 			displayTime();
